9558fdbd8ee0d0c43c1852e47c42fcbffbca9968
abolfazl <ab.eslami2001@gmail.com>
2026-09-01 19:09:10 +0330

    add least connection selection algorithm

---
+++ least_connection_selection.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type LeastConnSel struct {
+	mh *MinHeap
+}
+
+func NewLeastConnSel(bacnends []string) *LeastConnSel {
+	minHeap := NewMinHeap()
+	for _, b := range bacnends {
+		minHeap.InsertOrUpdate(b, 0)
+	}
+	return &LeastConnSel{
+		mh: minHeap,
+	}
+}
+
+func (l *LeastConnSel) Pick() string {
+	key, _, ok := l.mh.GetMin()
+	if !ok {
+		panic("pool is empty")
+	}
+	l.mh.Add(key, 1)
+
+	return key
+}
+
+func (l *LeastConnSel) Done(backend string) {
+	l.mh.Add(backend, -1)
+}
+
+
+func (l *LeastConnSel) Status() {
+	items := l.mh.GetAllOrdered()
+	for _, item := range items {
+		fmt.Printf("%s:%d\n", item.Key, item.Value)
+	}
+}
--- main.go
+++ main.go
@@ -4,114 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 	"strings"
 )
 
-type RoundRobin struct {
-	backands []string
-	index    int
-}
 
-func NewRoundRobin(backends []string) *RoundRobin {
-	return &RoundRobin{
-		backands: backends,
-		index:    0,
-	}
-}
-
-func (r *RoundRobin) PICK() string {
-	if len(r.backands) == 0 {
-		return ""
-	}
-	r.index = r.index % len(r.backands)
-	backend := r.backands[r.index]
-	r.index += 1
-	return backend
-}
-
-func (r *RoundRobin) REST() {
-	r.index = 0
-}
-
-type SmoothWeightedRR struct {
-	backends    []string
-	weights     []int
-	current     []int
-	totalWeight int
-}
-
-func (s *SmoothWeightedRR) PICK() string {
-	ans := s.PICKN("1")
-
-	return ans[0]
-}
-
-func (s *SmoothWeightedRR) PICKN(n string) []string {
-	N, err := strconv.Atoi(n)
-	if err != nil {
-		panic("Invalid argument for PICKN. Expected an integer.")
-	}
-	ans := make([]string, 0, N)
-	for i := 0; i < N; i++ {
-		p := Max(s.current)
-
-	InerLoop:
-		for j, key := range s.backends {
-			if s.current[j] == p {
-				ans = append(ans, key)
-				s.current[j] -= s.totalWeight
-				break InerLoop
-			}
-		}
-		for j := 0; j < len(s.current); j++ {
-			s.current[j] += s.weights[j]
-		}
-
-	}
-	return ans
-}
-
-func Max(in []int) int {
-	ans := -1000000000000
-	for i := 0; i < len(in); i++ {
-		if in[i] > ans {
-			ans = in[i]
-		}
-	}
-	return ans
-}
-
-func NewSmoothWeightedRR(backends []string) *SmoothWeightedRR {
-	backendList := make([]string, len(backends))
-	current := make([]int, len(backends))
-	weights := make([]int, len(backends))
-	totalWeight := 0
-	for i, backend := range backends {
-		parts := strings.Split(backend, ":")
-		if len(parts) == 2 {
-			weight, err := strconv.Atoi(parts[1])
-			if err != nil {
-				panic("Invalid backend weight. Expected an integer.")
-			}
-			backendList[i] = parts[0]
-			current[i] = weight
-			weights[i] = weight
-			totalWeight += weight
-		} else {
-			panic("Invalid backend format. Expected 'backend:weight'")
-		}
-	}
-	return &SmoothWeightedRR{
-		backends:    backendList,
-		current:     current,
-		weights:     weights,
-		totalWeight: totalWeight,
-	}
-}
 
 func main() {
-	// file, err := os.Open("tests/02-weighted-rr/4.in")
+	// file, err := os.Open("tests/03-least-connections/4.in")
 	// if err != nil {
 	// 	panic(err)
 	// }
@@ -119,8 +18,11 @@ func main() {
 	// sc := bufio.NewScanner(file)
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Buffer(make([]byte, 1024*1024), 1024*1024)
-	var swrr *SmoothWeightedRR
+	var lb *LeastConnSel
 	for sc.Scan() {
+		if sc.Err() != nil{
+			panic("error in scaning")
+		}
 		line := sc.Text()
 		if line == "" {
 			continue
@@ -128,14 +30,16 @@ func main() {
 		args := strings.Split(line, " ")
 		switch args[0] {
 		case "POOL":
-			swrr = NewSmoothWeightedRR(args[1:])
+			lb = NewLeastConnSel(args[1:])
 			fmt.Println("OK")
-		case "PICKN":
-			ans := swrr.PICKN(args[1])
-			fmt.Println(strings.Join(ans, ","))
+		case "DONE":
+			lb.Done(args[1])
+			fmt.Println("OK")
 		case "PICK":
-			ans := swrr.PICK()
+			ans := lb.Pick()
 			fmt.Println(ans)
+		case "STATUS":
+			lb.Status()
 		default:
 			fmt.Println("wrong input:", args[0])
 		}
